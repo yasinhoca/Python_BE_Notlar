14026a7715eec90bbd5ca2860bacd80e0889c819
Yasin Hoca <myasinozsaglam@gmail.com>
2025-09-04 10:49:34 +0300

    Add files via upload

---
+++ sayaclı while dongusu.py
@@ -0,0 +1,12 @@
+#0 ile 100 arasında 10 tane sayı oluşturup bunları ekrana yazan program - while döngüsü ile
+import random
+
+sayac = 0 
+while sayac<10:
+    a = random.randint(0,100)
+    print(sayac,". sayı = ", a)
+    sayac=sayac + 1 
+    
+    
+    
+
